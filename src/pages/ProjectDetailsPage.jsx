19d59a42f894cce0296fb4b7d8cdc7c4fe59f3f6
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-30 14:32:07 +0200

    feat: add functionality to delete projects

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -1,45 +1,58 @@
 import axios from "axios";
- import { Link, useParams } from "react-router-dom";
- import { API_URL } from "../config/api";
- import { useEffect, useState } from "react";
- import Loader from "../components/Loader";
- 
- 
- function ProjectDetailsPage() {
- 
-     const [project, setProject] = useState(null);
- 
-     const { projectId } = useParams();
- 
-     useEffect(() => {
-         axios.get(`${API_URL}/projects/${projectId}`)
-             .then(response => {
-                 setProject(response.data);
-             })
-             .catch((error) => console.log("Error getting project details from the API...", error));
-     }, [projectId]);
- 
- 
-     if(project === null) {
-         return <Loader />
-     }
- 
- 
-     return (
-         <div className="ProjectDetailsPage">
-             <h1>{project.title}</h1>
-             <p>{project.description}</p>
- 
-             <Link to="/projects">
-                 <button>Back to projects</button>
-             </Link>
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { API_URL } from "../config/api";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+
+
+function ProjectDetailsPage() {
+
+    const [project, setProject] = useState(null);
+
+    const { projectId } = useParams();
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get(`${API_URL}/projects/${projectId}`)
+            .then(response => {
+                setProject(response.data);
+            })
+            .catch((error) => console.log("Error getting project details from the API...", error));
+    }, [projectId]);
+
+
+
+    const deleteProject = () => {
+        axios.delete(`${API_URL}/projects/${projectId}`)
+            .then(response => {
+                navigate("/projects");
+            })
+            .catch((error) => console.log("Error deleting project...", error));
+    }
+
+
+    if (project === null) {
+        return <Loader />
+    }
+
+
+    return (
+        <div className="ProjectDetailsPage">
+            <h1>{project.title}</h1>
+            <p>{project.description}</p>
+
+            <Link to="/projects">
+                <button>Back to projects</button>
+            </Link>
 
             <Link to={`/projects/edit/${project.id}`}>
                 <button>Edit</button>
             </Link>
 
-         </div>
-     );
- }
- 
- export default ProjectDetailsPage;
+            <button onClick={deleteProject}>Delete</button>
+
+        </div>
+    );
+}
+
+export default ProjectDetailsPage;
