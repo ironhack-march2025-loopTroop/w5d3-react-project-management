6fc211aadd46c347242fcc8a4bda2fe4d60fc7f7
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-30 11:12:02 +0200

    feat: implement list of projects

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { API_URL } from "../config/api"
+
+import Loader from "../components/Loader"
+
+
 function ProjectListPage() {
+
+    const [projects, setProjects] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${API_URL}/projects`)
+            .then((response) => {
+                setProjects(response.data)
+            })
+            .catch(e => console.log("Error getting projects from the API...", e));
+    }, [])
+
+    if (projects === null) {
+        return <Loader />
+    }
+
     return (
         <div>
-            <h1>Project List Page</h1>
+            <h1>Number of projects: {projects.length}</h1>
+
+            {projects.map((projectObj) => {
+                return (
+                    <div className="card" key={projectObj.id}>
+                        <h3>{projectObj.title}</h3>
+                        <p>{projectObj.description}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
