3418913da38c3d0e1b3a69070c64f85bd20caf43
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-30 12:03:41 +0200

    feat: implement functionality to create new projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import ProjectListPage from "./pages/ProjectListPage"
+import CreateProjectPage from "./pages/CreateProjectPage"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/create" element={<CreateProjectPage />} />
         <Route path="/*" element={<h2>Page not found</h2>} />
       </Routes>
     </>
